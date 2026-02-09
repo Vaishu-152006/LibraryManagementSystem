cb81f10f91da050074de270151c223f30cc8c357
Vaishnavi J <Vaishnavi J@DESKTOP-3TPM999>
2026-02-09 11:35:31 +0530

    commit

---
+++ src/main/java/com/wipro/book/service/Administrator.java
@@ -0,0 +1,33 @@
+package com.wipro.book.service;
+
+import com.wipro.book.bean.BookBean;
+import com.wipro.book.dao.BookDAO;
+
+public class Administrator {
+	public String addBook(BookBean bookBean) {
+	    if (bookBean == null ||
+	        bookBean.getBookName().isEmpty() ||
+	        bookBean.getIsbn().isEmpty() ||
+	        bookBean.getCost() <= 0 ||
+	        bookBean.getAuthor() == null ||
+	        bookBean.getAuthor().getAuthorName().isEmpty() ||
+	        (bookBean.getBookType() != 'G' && bookBean.getBookType() != 'T')) {
+	        return "INVALID";
+	    }
+
+	    int res = new BookDAO().createBook(bookBean);
+	    if (res == 1) {
+	        return "SUCCESS";
+	    }
+	    return "FAILURE";
+	}
+
+	public BookBean viewBook(String isbn) {
+		if(isbn.isEmpty()) {
+			return null;
+		}
+		BookDAO bookDao=new BookDAO();
+		return bookDao.fetchBook(isbn);
+	}
+	
+}
